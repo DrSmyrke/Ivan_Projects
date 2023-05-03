203d9d3ebdb198a69bfdd2d52c8314955196bdf9
DrSmyrke <smyrke2005@yandex.ru>
2023-05-03 23:21:09 +0300

    Коментирование

--- Zagadka1/include/functions.h
+++ Zagadka1/include/functions.h
@@ -11,9 +11,21 @@ uint8_t isPressButton(const uint8_t pin);
 void onLamp(const uint8_t lampNum);
 void offLamp(const uint8_t lampNum);
 void setLed(const uint8_t ledNum, uint8_t r, uint8_t g, uint8_t b);
+/**
+ * Стадия показа ключа
+*/
 void setStage1(void);
+/**
+ * Переход в стадию отгадывания (перемешевание ключа)
+*/
 void setStage2(void);
+/**
+ * Процесс отгадывания загадки
+*/
 void processStage2(const uint8_t button);
+/**
+ * Стадия анимации решенной загадки с последующим открыванием
+*/
 void setStage3(void);
 void setStageError(void);
 void beep(const uint16_t on, const uint16_t off = 0);
